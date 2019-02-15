2e22160de0df743b9da29a52400d7dd6ada77cba
korvin <47597209+korvin55@users.noreply.github.com>
2019-02-15 21:52:42 +0300

    /

--- Population.cs
+++ Population.cs
@@ -6,6 +6,10 @@ namespace PadawansTask1
     {
         public static int GetYears(int initialPopulation, double percent, int visitors, int currentPopulation)
         {
+            if (initialPopulation<= 0 || currentPopulation <= 0)
+            {
+                throw new ArgumentException();
+            }
             int populationNow = initialPopulation;
             int year = 0;
             while (populationNow < currentPopulation)
